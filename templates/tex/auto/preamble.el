9b028d2b5010595f7f9ca9a929ee7fabcba39757
KrotosBenjamin <kj.benjamin90@gmail.com>
2025-10-25 17:22:27 -0500

    updated based on testing

--- templates/tex/auto/preamble.el
+++ templates/tex/auto/preamble.el
@@ -4,13 +4,13 @@
  "preamble"
  (lambda ()
    (TeX-add-to-alist 'LaTeX-provided-package-options
-                     '(("helvet" "scaled") ("setspace" "") ("geometry" "") ("hyperref" "hidelinks") ("microtype" "") ("fancyhdr" "")))
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
-   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+                     '(("helvet" "scaled") ("setspace" "") ("geometry" "") ("hyperref" "hidelinks") ("microtype" "") ("titlesec" "")))
    (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "href")
    (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
    (TeX-run-style-hooks
     "helvet"
@@ -18,6 +18,6 @@
     "geometry"
     "hyperref"
     "microtype"
-    "fancyhdr"))
+    "titlesec"))
  :latex)
 
